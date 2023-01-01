ab367b54fc274a3244f9021871154777cf9dfe2b
stalvok <80374859+stalvok@users.noreply.github.com>
2023-01-01 23:31:53 +0300

    Update WelcomePage.vue

--- src/components/WelcomePage.vue
+++ src/components/WelcomePage.vue
@@ -3,7 +3,7 @@
     <div class="w-full absolute brightness-50 bg-no-repeat h-full"></div>
     <div class="container h-full mx-auto flex justify-center items-end lg:items-center px-6">
         <div class="flex mb-10 z-20 relative text-white text-center max-w-[420px] flex-col gap-10">
-          <div class="font-bold lg:text-5xl text-3xl">Welcome to Move</div>
+          <div class="font-bold text-red lg:text-5xl text-3xl">Welcome to Move</div>
           <div class="text-lg lg:text-2xl">the best movie streaming service of the century to make your days great!</div>
           <router-link to="/login">
             <div class="button login">
